175daca927419946be7bd2e8436ccfe32200a2c5
Elenger <orion96king@gmail.com>
2020-07-16 21:19:33 +0200

    Add script MobNowMesh. Add script MobCreation.

---
+++ Assets/Scripts/Mob/MobNavMesh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobNavMesh : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+    public Transform target;
+
+    private void Update()
+    {
+        _navMeshAgent.SetDestination(target.position);
+    }  
+}
