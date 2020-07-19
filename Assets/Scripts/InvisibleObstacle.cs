8b9d6117a4029eb9d93612790ce47cb5f613bfc5
Elenger <orion96king@gmail.com>
2020-07-19 15:03:39 +0200

    Change scene

---
+++ Assets/Scripts/InvisibleObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleObstacle : MonoBehaviour
+{
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] Transform _target;
+
+    private void Update()
+    {
+        Ray rayCamera = Camera.main.ScreenPointToRay(_target.position);
+
+        if (Physics.Raycast(rayCamera, out RaycastHit hit, _layerMask))
+        {
+            Debug.Log(hit);
+            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = hit.transform.gameObject.GetComponent<MeshRenderer>().material.color * 0.5f;
+        }
+    }
+}
